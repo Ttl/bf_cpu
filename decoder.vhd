41eb39e7d3bc62602e42fae925095fe31ab0d167
Henrik Forstén <henrik.forsten@gmail.com>
2013-03-10 21:33:45 +0200

    New control signals

--- decoder.vhd
+++ decoder.vhd
@@ -12,52 +12,77 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity decoder is
     Port ( instr : in  STD_LOGIC_VECTOR(7 downto 0);
-           d_jumpf : out  STD_LOGIC;
-           d_jumpb : out  STD_LOGIC;
-           d_add : out  STD_LOGIC;
-           d_sub : out  STD_LOGIC;
-           d_inc : out STD_LOGIC;
-           d_dec : out STD_LOGIC;
-           d_imm : out  STD_LOGIC_VECTOR (7 downto 0);
-           d_read : out  STD_LOGIC;
-           d_write : out  STD_LOGIC;
-           d_step : out STD_LOGIC_VECTOR(3 downto 0));
+           d_alutoreg : out STD_LOGIC;
+           d_alua : out STD_LOGIC_VECTOR(1 downto 0);
+           d_alub : out STD_LOGIC_VECTOR(1 downto 0);
+           d_write : out STD_LOGIC;
+           d_aluop : out STD_LOGIC_VECTOR(1 downto 0);
+           d_jumpf : out STD_LOGIC;
+           d_jumpb : out STD_LOGIC
+           );
 end decoder;
 
 architecture Behavioral of decoder is
 
 begin
+d_alutoreg <= '0';
+d_alub <= "00";
+d_alua <= "00";
+d_write <= '0';
+d_aluop <= "00";
 d_jumpf <= '0';
 d_jumpb <= '0';
-d_add <= '0';
-d_sub <= '0';
-d_inc <= '0';
-d_dec <= '0';
-d_imm <= (others => '0');
-d_read <= '0';
-d_write <= '0';
-d_step <= "0001";
 
 process(instr)
 begin
+
+
+-- d_aluop
+-- 00 : add
+-- 01 : illegal
+-- 10 : pass a
+-- 11 : pass b
+
+-- d_alutoreg
+-- 0 : write alu result to mem(pointer)
+-- 1 : write alu result to pointer
+
+-- d_alua
+-- 00 : mem(pointer)
+-- 01 : pointer
+-- 10 : read from input
+-- 11 : illegal
+
+-- d_alub
+-- 00 : 0
+-- 01 : 1
+-- 10 : -1
+-- 11 : illegal
+
+-- d_write
+-- 1 : write alu result to output
 
 case instr is
     
     -- <
     when x"3C" =>
-        d_dec <= '1';
+        d_alutoreg <= '1';
+        d_alua <= "01";
+        d_alub <= "10";
     
     -- >
     when x"3E" =>
-        d_inc <= '1';
+        d_alutoreg <= '1';
+        d_alua <= "01";
+        d_alub <= "01";
     
     -- +
     when x"2B" =>
-        d_add <= '1';
+        d_alub <= "01";
     
     -- -
     when x"2D" =>
-        d_sub <= '1';
+        d_alub <= "10";
     
     -- .
     when x"2E" =>
@@ -65,7 +90,7 @@ case instr is
     
     -- ,
     when x"2C" =>
-        d_read <= '1';
+        d_alua <= "11";
         
     -- [
     when x"5B" =>
@@ -74,6 +99,9 @@ case instr is
     -- ]
     when x"5D" =>
         d_jumpb <= '1';
+        
+    when others =>
+        -- nop
         
 end case;
 
