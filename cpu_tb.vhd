8282f0c699b55bdd21665316c28cd54fc48e9e49
Henrik Forstén <henrik.forsten@gmail.com>
2013-03-17 20:53:58 +0200

    Add UART to testbench

--- cpu_tb.vhd
+++ cpu_tb.vhd
@@ -3,6 +3,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
+USE std.textio.all;
 
 ENTITY cpu_tb IS
 END cpu_tb;
@@ -13,11 +14,13 @@ signal clk, reset, tx, rx : std_logic;
 
 -- Clock period definitions
 constant clk_period : time := 10 ns;
-   
+
+signal uart_tx_req, uart_tx_end, uart_rx_ready : std_logic;
+signal uart_tx_data, uart_rx_data : std_logic_vector(7 downto 0);
 BEGIN
 -- Component Instantiation
       uut: entity work.cpu
-      Generic map ( INSTRUCTIONS => "scripts/branch.mif"
+      Generic map ( INSTRUCTIONS => "scripts/hello.mif"
       )
       Port map(clk => clk,
                reset => reset,
@@ -25,6 +28,35 @@ BEGIN
                rx => tx
       );
 
+    uart1 : entity work.uart
+    Generic map(
+        CLK_FREQ => 100,
+        SER_FREQ => 1000000,
+        PARITY_BIT => false
+    )
+    Port map (
+        clk	=> clk,
+        rst	=> reset,
+        rx => rx,
+        tx => tx,
+        tx_req => uart_tx_req,
+        tx_end => uart_tx_end,
+        tx_data	=> uart_tx_data,
+        rx_ready => uart_rx_ready,
+        rx_data	=> uart_rx_data
+    );
+
+    -- Print received bytes
+    uart_process : process
+    begin
+        wait until uart_rx_ready = '1';
+        wait for clk_period;
+        if to_integer(unsigned(uart_rx_data)) > 31 and to_integer(unsigned(uart_rx_data)) < 127 then
+            report "Received ASCII: "&character'image(character'val(to_integer(unsigned(uart_rx_data))));
+        else
+            report "Received Dec: "&integer'image(to_integer(unsigned(uart_rx_data)));
+        end if;
+    end process;
    -- Clock process definitions
    clk_process :process
    begin
@@ -38,7 +70,7 @@ BEGIN
  tb : PROCESS
  BEGIN
     reset <= '1';
-    tx <= '0';
+    uart_tx_req <= '0';
     wait for 100 ns; -- wait until global set/reset completes
     reset <= '0';
     
