4d8a8c53ce12e32d7ebe6993e59ecd0722ba6dc8
Henrik Forstén <henrik.forsten@gmail.com>
2013-03-18 20:04:15 +0200

    Don't write newlines

--- scripts/bftomif.py
+++ scripts/bftomif.py
@@ -4,4 +4,5 @@ with open(sys.argv[1],'r') as bf:
     with open(sys.argv[2],'w') as outfile:
         for line in bf:
             for char in line:
-                outfile.write(bin(ord(char))[2:].zfill(8)+' -- '+char+'\n')
+                if char != '\n':
+                    outfile.write(bin(ord(char))[2:].zfill(8)+' -- '+char+'\n')
