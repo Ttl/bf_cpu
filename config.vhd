a35f7910dae1ba766cd0c159fa98057752376b98
Henrik Forstén <henrik.forsten@gmail.com>
2013-03-19 00:04:39 +0200

    Change config for mandelbrot

--- config.vhd
+++ config.vhd
@@ -6,7 +6,7 @@ package bfconfig is
     constant INST_MEM_SIZE : integer := 14;
     constant JUMPF_CACHE_SIZE : integer := 4; -- Log2 of cache size
     constant STACK_SIZE : integer := 8;
-    constant REG_SIZE : integer := 16;
+    constant REG_SIZE : integer := 9;
     
     subtype pctype is std_logic_vector(INST_MEM_SIZE-1 downto 0);
     subtype pointertype is std_logic_vector(REG_SIZE-1 downto 0);
