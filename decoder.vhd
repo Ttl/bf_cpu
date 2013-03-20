e228eb12eea168db780ab79755ee5c650b67e307
Henrik Forstén <henrik.forsten@gmail.com>
2013-03-20 20:17:46 +0200

    Comments

--- decoder.vhd
+++ decoder.vhd
@@ -1,14 +1,9 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- Decoder reads the instruction from RAM and outputs
+-- the datapath control signals for executing that
+-- instruction
 
 entity decoder is
     Port ( instr : in  STD_LOGIC_VECTOR(7 downto 0);
